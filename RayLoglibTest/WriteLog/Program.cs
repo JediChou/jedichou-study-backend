1328a5e88e0f61d11c6217e7d0ef7d117754382e
jedichou <jedigit@163.com>
2020-01-03 17:18:21 +0800

    增加SX中的日志调试项目

---
+++ RayLoglibTest/WriteLog/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SXLibrary;
+using System.Threading;
+
+
+namespace WriteLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i=0; i<10; i++)
+            {
+                //var strLogMessage = string.Format(
+                //    "{0} - Jedi write log message", 
+                //    DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")
+                //);
+                Log.WriteDebugLog("debug" + i);
+                Log.WriteErrorLog("error" + i);
+                Log.WriteInfoLog("info" + i);
+                //Thread.Sleep(5);
+            }
+        }
+    }
+}
