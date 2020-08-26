7a6a5774fbb596d2e153a126295da91cc8109c89
jedichou <jedigit@163.com>
2020-08-26 15:39:23 +0800

    增加一个简单的copy示例代码

---
+++ csharp-skills/SimpCp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+class Program {
+    static void Main (string[] args) {
+        string src = @"d:\test1.txt", tgt = @"d:\test2.txt";
+        using (var fs = new FileStream (src, FileMode.Open)) {
+            CopyStream (fs, tgt);
+        }
+    }
+
+    static void CopyStream (Stream stream, string destPath) {
+        using (var fs = new FileStream (destPath, FileMode.Create, FileAccess.Write))
+        stream.CopyTo (fs);
+    }
+}
