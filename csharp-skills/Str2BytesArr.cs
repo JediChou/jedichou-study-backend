1c68119804ba9260abdd4ccec356d606f3defb41
jedichou <jedigit@163.com>
2020-08-26 15:22:13 +0800

    增加Str转Byte数组的示例

---
+++ csharp-skills/Str2BytesArr.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Define a normal string object
+        var str = "Jedi use vscode to execute a test. 中文内容呢？";
+        System.Console.WriteLine($"str content: {str}");
+        
+        // Get str's byte array, and output to console.
+        var bytesArr = GetBytes(str);
+        System.Console.Write("str content(byte): ");
+        foreach (var c in bytesArr)
+            System.Console.Write(c);
+        System.Console.WriteLine();
+
+        // Get string content from byte array.
+        var strCopy = GetString(bytesArr);
+        System.Console.WriteLine($"strCopy content: {strCopy}");
+    }
+
+    static byte[] GetBytes(string str)
+    {
+        byte[] bytes = new byte[str.Length * sizeof(char)];
+        System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+        return bytes;
+    }
+
+    static string GetString(byte[] bytes)
+    {
+        char[] chars = new char[bytes.Length / sizeof(char)];
+        System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+        return new string(chars);
+    }
+}
