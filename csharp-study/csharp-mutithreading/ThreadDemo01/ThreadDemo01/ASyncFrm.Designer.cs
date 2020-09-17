8e2b2d7b2e6b5a979b24a135f0625591ef3e24f9
jedichou <jedigit@163.com>
2020-09-17 10:15:05 +0800

    增加CSharp多线程专题讨论的第一个例子（来自11的讲解）

---
+++ csharp-study/csharp-mutithreading/ThreadDemo01/ThreadDemo01/ASyncFrm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ThreadDemo01
+{
+    partial class ASyncFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSync = new System.Windows.Forms.Button();
+            this.btnASync = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(27, 40);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(75, 23);
+            this.btnSync.TabIndex = 0;
+            this.btnSync.Text = "同步调用";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // btnASync
+            // 
+            this.btnASync.Location = new System.Drawing.Point(27, 79);
+            this.btnASync.Name = "btnASync";
+            this.btnASync.Size = new System.Drawing.Size(75, 23);
+            this.btnASync.TabIndex = 1;
+            this.btnASync.Text = "异步调用";
+            this.btnASync.UseVisualStyleBackColor = true;
+            this.btnASync.Click += new System.EventHandler(this.btnASync_Click);
+            // 
+            // ASyncFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 150);
+            this.Controls.Add(this.btnASync);
+            this.Controls.Add(this.btnSync);
+            this.Name = "ASyncFrm";
+            this.Text = "ASyncFrm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Button btnASync;
+    }
+}
---
+++ csharp-study/csharp-mutithreading/ThreadDemo01/ThreadDemo01/ASyncFrm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadDemo01
+{
+    public partial class ASyncFrm : Form
+    {
+        public ASyncFrm()
+        {
+            InitializeComponent();
+            Console.WriteLine("*********** ThreadDemo01  ***********");
+            Console.WriteLine("Current thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private void btnSync_Click(object sender, EventArgs e)
+        {
+            Action<string> action = PostMessageToConsole;
+            Console.WriteLine("\nbtnSync click event...");
+            for (int i=0; i < 5; i++)
+            {
+                var name = $"btnSync -> {Thread.CurrentThread.ManagedThreadId}";
+                action.Invoke(name);
+            }
+        }
+
+        private void btnASync_Click(object sender, EventArgs e)
+        {
+            Action<string> action = PostMessageToConsole;
+            Console.WriteLine("\nbtnASync click event...");
+
+            for (int i = 0; i < 5; i++)
+            {
+                string name = $"btnASync -> {Thread.CurrentThread.ManagedThreadId}";
+                action.BeginInvoke(name, null, null);
+            }
+        }
+
+        /// <summary>
+        /// Post message to console
+        /// </summary>
+        /// <param name="msg">message</param>
+        private static void PostMessageToConsole(string msg)
+        {
+            // 消耗时间
+            int j = 0, k = 999999999, m = 0;
+            for (j = 0; j < k; j++)
+                m = j + k;
+            Console.Write(Thread.CurrentThread.ManagedThreadId + " ");
+            Console.WriteLine($"From PostMessageToConsole: {msg}");
+        }
+    }
+}
